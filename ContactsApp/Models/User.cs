1eb3e82d76258040b681e80f845fd298f4057822
Jack Harvey <JackHarvey.00@gmail.com>
2022-03-23 07:32:41 +1100

    Fix naming convention for model property

---
+++ ContactsApp/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactsApp.Models
+{
+    [Keyless]
+    public partial class User
+    {
+        [Column("userId")]
+        public int UserId { get; set; }
+        [Column("userName")]
+        [StringLength(255)]
+        public string UserName { get; set; } = null!;
+
+        [Column("themeSelection")]
+        [StringLength(255)]
+        public string? ThemeSelection { get; set; }
+    }
+}
