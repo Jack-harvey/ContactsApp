37c17d9928d7fffafa1c2735457021414632b8c2
Jack Harvey <JackHarvey.00@gmail.com>
2022-03-29 22:03:41 +1100

    Check user for theme preference on load of page

---
+++ ContactsApp/wwwroot/js/pageLoadThemeLoad.js
@@ -0,0 +1,9 @@
+﻿$(function () {
+    const usernameSelected = document.getElementById("userLoginSelect");
+    $.get("home/UsernameAndTheme", { userId: usernameSelected.value }, function (themePreference) {
+        console.log(themePreference);
+        if (themePreference[0].themeSelection == "atomOneDark") {
+            document.body.classList.toggle('atom-one-dark-theme',);
+        };
+    });
+});
