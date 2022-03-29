895d37862b786088b5b7e681619a4b115c3297d1
Jack Harvey <JackHarvey.00@gmail.com>
2022-03-29 22:17:35 +1100

    Load theme selection before page finishes

--- ContactsApp/wwwroot/js/pageLoadThemeLoad.js
+++ ContactsApp/wwwroot/js/pageLoadThemeLoad.js
@@ -1,9 +1,13 @@
-﻿$(function () {
+﻿//$(function () {
     const usernameSelected = document.getElementById("userLoginSelect");
     $.get("home/UsernameAndTheme", { userId: usernameSelected.value }, function (themePreference) {
         console.log(themePreference);
+        if (!themePreference[0].themeSelection == "atomOneDark" || !themePreference[0].themeSelection == "Dracula") {
+            return
+        };
         if (themePreference[0].themeSelection == "atomOneDark") {
             document.body.classList.toggle('atom-one-dark-theme',);
         };
+        document.body.classList.toggle('dracula-theme',);
     });
-});
+//});
