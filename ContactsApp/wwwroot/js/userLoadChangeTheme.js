b263ca69a82db4f513c3e65955c2a2f379695f3a
Jack Harvey <JackHarvey.00@gmail.com>
2022-04-05 07:51:12 +1000

    Add logic for selecting theme on user load

--- ContactsApp/wwwroot/js/userLoadChangeTheme.js
+++ ContactsApp/wwwroot/js/userLoadChangeTheme.js
@@ -1,5 +1,12 @@
 ﻿document.getElementById("userLoginSelect").addEventListener('change', function () {
         $.get("home/UsernameAndTheme", { userId: this.value }, function (themePreference) {
             console.log(themePreference);
+            if (!themePreference[0].themeSelection == "atomOneDark" || !themePreference[0].themeSelection == "Dracula") {
+                return
+            };
+            if (themePreference[0].themeSelection == "atomOneDark") {
+                document.body.classList.toggle('atom-one-dark-theme',);
+            };
+            document.body.classList.toggle('dracula-theme',);
         });
     });
